12c023fe8efca9f3aa73e3458a6f30ec9d06f67a
SsamuX <sgachoki559@gmail.com>
2026-04-05 18:54:45 +0300

    Add performance message function based on grade

--- messages.py
+++ messages.py
@@ -1,1 +1,11 @@
-
+def get_performance_message(grade):
+    if grade == "A":
+        return "Excellent performance!"
+    elif grade == "B":
+        return "Very good performance!"
+    elif grade == "C":
+        return "Good performance!"
+    elif grade == "D":
+        return "Fair performance. Improve more."
+    else:
+        return "Poor performance. Work harder."
