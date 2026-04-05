3ab9fe26f7cf85ed7216848d300df29e19bd42de
SsamuX <sgachoki559@gmail.com>
2026-04-05 18:52:55 +0300

    Add function to get student data and marks

--- input_handler.py
+++ input_handler.py
@@ -1,1 +1,11 @@
 
+def get_student_data():
+    name = input("Enter student name: ")
+    maths = float(input("Enter Maths marks: "))
+    english = float(input("Enter English marks: "))
+    science = float(input("Enter Science marks: "))
+    kiswahili = float(input("Enter Kiswahili marks: "))
+    social_studies = float(input("Enter Social Studies marks: "))
+
+    marks = [maths, english, science, kiswahili, social_studies]
+    return name, marks
