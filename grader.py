4e87b50b28681715c2b47088329fa4aca68566c4
SsamuX <sgachoki559@gmail.com>
2026-04-05 18:54:18 +0300

    Update grader.py

--- grader.py
+++ grader.py
@@ -1,1 +1,11 @@
-
+def get_grade(average):
+    if average >= 80:
+        return "A"
+    elif average >= 70:
+        return "B"
+    elif average >= 60:
+        return "C"
+    elif average >= 50:
+        return "D"
+    else:
+        return "E"
